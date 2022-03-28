dc1dd403a625c861633a6629abd0c670171514fd
5ime <31686695+5ime@users.noreply.github.com>
2022-03-28 19:53:50 +0800

    new

---
+++ README.md
@@ -0,0 +1,12 @@
+
+![预览图](cover.png)
+
+> 根据 nexmoe/hexo-theme-nexmoe 和 SaigyoujiYuyuko233/Saigyou_Ayakashi-HexoTheme 修改的主题
+
+## 修改记录
+
+- 封面高度100% -> 80%
+- 封面改为随机动漫图
+- 归档页面标签限制最大高度
+- 扩展Valine内置配置
+- 同步最新Dev版本
---
+++ source/js/app.js
@@ -0,0 +1,39 @@
+// 表格相册
+$("table")
+  .has("img")
+  .addClass("nexmoe-album");
+
+// 搜索
+function search() {
+  window.open($("#search_form").attr("action_e") + " " + $("#search_value").val());
+  return false;
+}
+
+// 平滑跳转同时修复锚点链接被转义
+$(document).ready(function () {
+  $("a.toc-link").click(function (ev) {
+    ev.preventDefault();
+    $("html, body").animate({
+      scrollTop: $(decodeURI($(this).attr("href"))).offset().top - 25
+    }, {
+      duration: 500,
+      easing: "swing"
+    });
+  });
+});
+
+$(document).on("copy", function(){
+  if(!window.copyTip){ return; }
+  var sel = document.getSelection();
+  var ele = document.createElement("div");
+  ele.innerHTML = '<div style="position: fixed;opacity: 0;white-space: pre;">' + sel + "\n\n" + window.copyTip.replaceAll("%url",document.location.href) + ' </div>' 
+  document.body.appendChild(ele);
+  sel.selectAllChildren(ele);
+  setTimeout(function () {document.body.removeChild(ele);});
+});
+
+/*随机封面图*/
+let covers = $(".post-rand-cover");
+for (let i = 0; i < covers.length; i++){
+  covers[i].dataset.src = "https://tenapi.cn/acg/?" + Math.round(Math.random() * (299-1));
+}
